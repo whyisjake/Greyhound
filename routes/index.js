14dbc5077ba612f85e982ff36842dd180d2b0c1f
Jake Spurlock <jake_spurlock@wired.com>
2015-01-17 16:24:57 -0500

    Video routes.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
-var express = require('express');
-var router = express.Router();
+var db = require("../database.js");
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+exports.index = function(req, res) {
+	res.render('index', {
+		title: 'Greyhound'
+	});
+};
 
-module.exports = router;
+
+// Let's start the videos.
+exports.videos = {};
+/*
+ * GET all videos
+ */
+exports.videos.all = function(req, res) {
+	db.videos.find(function(err, videos) {
+		if(err) return;
+		res.json(videos);
+	});
+};
+
+/*
+ * GET one video
+ */
+exports.videos.one = function(req, res) {
+	db.videos.findOne({ "_id" : db.ObjectId(req.params.id) }, function(err, video) {
+		if(err) return;
+		res.json(video);
+	});
+};
+
+/*
+ * POST a new player
+ */
+exports.videos.create = function(req, res) {
+	res.json(req.body);
+	db.videos.save(req.body);
+};
+
+exports.video = function(req, res) {
+	db.videos.findOne({ "_id" : db.ObjectId(req.params.id) }, function(err, video) {
+		if(err) return;
+		console.log(video);
+		res.render('single', {
+			post: video
+		});
+	});
+};
