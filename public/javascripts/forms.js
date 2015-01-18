1bcab1172e77b295059eae7d8b61ca6c19f6782b
Jake Spurlock <jake_spurlock@wired.com>
2015-01-18 08:23:40 -0500

    This doesn't do anything. Yet.

--- public/javascripts/forms.js
+++ public/javascripts/forms.js
@@ -26,4 +26,33 @@ jQuery(document).ready(function(){
 		});
 
 	});
+
+	jQuery('.search-reddit .submit').click(function(e) {
+
+		console.log('Clicked!');
+
+		// Keep the button from being clicked
+		e.preventDefault();
+
+		var $inputs = $('.addVideo :input'),
+			form = {};
+
+		$inputs.each(function() {
+			form[this.name] = $(this).val();
+		});
+
+		console.log(form);
+
+		// Make the ajax request with the form data.
+		$.ajax({
+			url: '/videos',
+			data: form,
+			type: 'POST',
+			success: function(res) {
+				console.log( res );
+			}
+		});
+
+	});
+
 });
