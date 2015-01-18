67f5ab2d1b9423bcb9e6864c9267911d67771315
Jake Spurlock <jake_spurlock@wired.com>
2015-01-18 00:26:14 -0500

    Let's update the db url.

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-var dbUrl = "greyhound";
+var dbUrl = "mongodb://PnUNoXnxdsxT:dfEVbcHFAFmw@mongosoup-cont002.mongosoup.de:31516/cc_PnUNoXnxdsxT";
 var collections = ["videos", "links"];
 
 var db = require("mongojs").connect(dbUrl, collections);
