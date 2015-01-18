10d44be6760c9feef9abf9a68bc4c483591262e6
Jake Spurlock <jake_spurlock@wired.com>
2015-01-18 01:32:01 -0500

    Set the correct db url on prod/local.

--- database.js
+++ database.js
@@ -1,4 +1,9 @@
+// PROD DB URL
 var dbUrl = "mongodb://PnUNoXnxdsxT:dfEVbcHFAFmw@mongosoup-cont002.mongosoup.de:31516/cc_PnUNoXnxdsxT";
+
+// DEV URL
+// var dbUrl = "greyhound"
+
 var collections = ["videos", "links"];
 
 var db = require("mongojs").connect(dbUrl, collections);
