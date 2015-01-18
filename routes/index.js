97042e7860eb26aec1f8d5ae3bde80d5fcd309db
Jake Spurlock <jake_spurlock@wired.com>
2015-01-17 19:34:58 -0500

    Clean some things up.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var db = require("../database.js");
+var embedly = require('embedly');
+var util = require('util');
 
 exports.index = function(req, res) {
 	res.render('index', {
@@ -18,7 +20,10 @@ exports.videos = {};
  */
 exports.videos.all = function(req, res) {
 	db.videos.find(function(err, videos) {
-		if(err) return;
+		if(err) {
+			return;
+		}
+
 		res.json(videos);
 	});
 };
@@ -47,19 +52,23 @@ exports.video = function(req, res) {
 
 		var EMBEDLY_KEY = 'f8fe2d9f411b4bac9d6b89454b1c4ebc';
 
-		var embedly = require('embedly'),
-			util = require('util');
-
 		var myObj;
 
 		new embedly({key: EMBEDLY_KEY}, function(err, api) {
 
 			// call single url
 			var url = video.url;
+
 			obj = api.oembed({url: url}, function(err, objs) {
 
 				video.embed = objs[0].html;
+
+				// Did we get the embed?
 				console.log(video.embed);
+
+				// Let's update the DB with the embed.ly info.
+				db.videos.save(video);
+
 				res.render('single', {
 					post: video
 				});
